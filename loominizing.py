6b2d4ac2742a6473529e6893149f41531daa708e
Aldo Delgado Pimentel <106998666+Aldo380@users.noreply.github.com>
2025-03-29 23:49:58 +0000

    Commit one

---
+++ loominizing.py
@@ -0,0 +1,29 @@
+class LoomisModel(nn.Module):
+    def __init__(self):
+        super(LoomisModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 32 * 8 * 8)
+
+            x = F.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
+
+
+
+
+
+##Loss function, optimizer:
+
+import torch.optim as optim
+
+model = LoomisModel()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
